dbbcc015b58597438aca6702c6921d954b84ed95
milandey2006 <deymilan066@gmail.com>
2025-07-25 19:35:46 +0530

    credits

--- component/testimonials.js
+++ component/testimonials.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Card, CardContent } from "@/components/ui/card";
 import { ShieldCheck } from "lucide-react"; // Using a thematic icon
 
-// Testimonial data array
+
 const testimonials = [
   {
     quote: "Champion Security system is an excellent organisation having in depth and sound knowledge and experience in CCTV network and we at Malkauns CHS are extremely happy with their installation and commissioning of 21 cameras in our premises with 2 in elevator lifts.",
     author: "Chandrashekhar Saple",
-    // role: "Small Business Owner"
+
   },
   {
     quote: "Owner of this store, Mr Rajesh Dey is very genuine. He will guide you as per your requirements. Provide you a quality service at very affordable rates. No cheating at all.. Definitely recommended for video door phone services..",
     author: "Sachin Giri",
-    // role: "Homeowner"
+
   }
 ];
 
